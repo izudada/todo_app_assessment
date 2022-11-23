d6a996e3487960a21a6aedf3fbc105853e67f94e
Anthony <tonyudeagbala@gmail.com>
2022-11-24 00:08:00 +0100

    chore: create Todo model

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Todo(models.Model):
+    title = models.CharField(max_length=200)
+    body = models.TextField()
+    completed = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    edited_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return str(self.title)
