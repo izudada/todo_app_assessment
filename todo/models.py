a3a60895e67b4de85e93dfcca772831c724d99f0
Anthony <tonyudeagbala@gmail.com>
2022-11-24 13:35:52 +0100

    chore: make title unique

--- todo/models.py
+++ todo/models.py
@@ -1,8 +1,9 @@
 from django.db import models
+from django.urls import reverse
 
 
 class Todo(models.Model):
-    title = models.CharField(max_length=200)
+    title = models.CharField(max_length=200, unique=True)
     body = models.TextField()
     completed = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
@@ -10,3 +11,6 @@ class Todo(models.Model):
 
     def __str__(self):
         return str(self.title)
+    
+    def get_absolute_url(self):
+        return reverse('index')
