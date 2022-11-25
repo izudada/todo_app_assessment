b9e8386415d5fbeacf13fdd955c2dbf76f2a1d7e
Anthony <tonyudeagbala@gmail.com>
2022-11-25 11:14:35 +0100

    fix: allowed host

--- config/settings.py
+++ config/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['iz-todo-assessment.herokuapp.com']
 
 
 # Application definition
